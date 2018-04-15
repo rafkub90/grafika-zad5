80d92f69bce837af905a8ba0166b076f8e96ba60
DESKTOP-HUMKCOQ\rafku <rafkub90@gmail.com>
2018-04-15 23:29:07 +0200

    no message

---
+++ Assets/trigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class trigger : MonoBehaviour
+{
+
+    private Animator _animator = null;
+    public CapsuleCollider player = null;
+    private GameObject[] playerTag;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        _animator.SetBool("isClosed", false);
+        Debug.Log("OPEN");
+
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        _animator.SetBool("isClosed", true);
+        Debug.Log("Close");
+
+
+    }
+}
